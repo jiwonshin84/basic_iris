01ada5c8a73e6fa7a84d2760457907f491ed7c98
jiwonshin84 <jiwonshin84@gmail.com>
2025-04-24 14:02:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -24,3 +24,39 @@ sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.8)
 sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.1)
 petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 3.8)
 petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 1.2)
+
+
+# 예측 모델 생성
+# Separate X and y
+X = df.drop('Species', axis=1)
+y = df.Species
+
+# Data Splitting
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Model building
+rf = RandomForestClassifier(max_depth=2, max_features=4, n_estimators=200, random_state=42)
+rf.fit(X_train, y_train)
+
+# Apply Model to make predictions
+y_pred = rf.predict([[sepal_length, sepal_width, petal_length, petal_width]])
+st.write(y_pred)
+
+# 간단한 EDA_ 아이리스 종에 따른 4개 컬럼 평균 계산
+
+# print EDA
+st.subheader('Brief EDA')
+st.write('The data is grouped by the class and the variable mean is computed for each class.')
+groupby_species_mean = df.groupby('Species').mean()
+st.write(groupby_species_mean)
+
+# 슬라이더에 Input 한 컬럼 값을 데이터프레임으로 출력하기
+# print input Features
+input_feature = pd.DataFrame([[sepal_length, sepal_width, petal_length, petal_width]],
+                              columns = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width'])
+st.write(input_feature)
+
+# 예측 결과값을 metric으로 출력
+st.subheader('Output')
+st.metric('Predicted class', y_pred[0], '')
+
