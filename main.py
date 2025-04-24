92da7b3132ba4e2385194027be38c50a8de1c491
jiwonshin84 <jiwonshin84@gmail.com>
2025-04-24 13:53:05 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
+# Page configuration
+st.set_page_config(
+    page_title='Simple Prediction App',
+    page_icon='🌷',
+    layout='wide',
+    initial_sidebar_state='expanded'
+)
+
+# title of app
+st.title('🌷 Simple Prediction App')
+
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/iris.csv')
+st.write(df)
+
+# input widgets
+st.sidebar.subheader('Input Features')
+sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.8)
+sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.1)
+petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 3.8)
+petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 1.2)
