4b85b956971e57ce9b8a51a1e7186ea3667bff93
jiwonshin84 <jiwonshin84@gmail.com>
2025-04-25 11:16:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -38,23 +38,7 @@ if st.sidebar.button('🎉 Brief EDA'):
     st.write(groupby_species_mean)
 
 
-# 예측 모델 생성
-# Separate X and y
-X = df.drop('Species', axis=1)
-y = df.Species
 
-# Data Splitting
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Model building
-rf = RandomForestClassifier(max_depth=2, max_features=4, n_estimators=200, random_state=42)
-rf.fit(X_train, y_train)
-
-st.subheader('💡 Input Features값 예측')
-
-# Apply Model to make predictions
-y_pred = rf.predict([[sepal_length, sepal_width, petal_length, petal_width]])
-# st.write(y_pred)
 
 
 if st.sidebar.button('📊 Confusion Matrix'):
@@ -86,6 +70,23 @@ if st.session_state.show_slider:
     petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 3.8)
     petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 1.2)
 
+    # 예측 모델 생성
+    # Separate X and y
+    X = df.drop('Species', axis=1)
+    y = df.Species
+    
+    # Data Splitting
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    
+    # Model building
+    rf = RandomForestClassifier(max_depth=2, max_features=4, n_estimators=200, random_state=42)
+    rf.fit(X_train, y_train)
+    
+    st.subheader('💡 Input Features값 예측')
+    
+    # Apply Model to make predictions
+    y_pred = rf.predict([[sepal_length, sepal_width, petal_length, petal_width]])
+    # st.write(y_pred)
     
     # 슬라이더에 Input 한 컬럼 값을 데이터프레임으로 출력하기
     # print input Features
